8f761f47d0850fe7681fe75c1b29d29b20681255
AlgerDu <AlgerDu@outlook.com>
2023-11-20 15:11:43 +0800

    添加注释

--- src/dinfra/config.go
+++ src/dinfra/config.go
@@ -3,15 +3,22 @@ package dinfra
 import di "github.com/AlgerDu/go-di/src"
 
 type (
+	OnChangeFunc func() error
+
+	// 配置
 	Config interface {
+		// 将配置绑定到结构体
 		BindStruct(path string, dst any) error
 	}
 
-	ConfigChangeListener interface {
-		OnChange() error
+	// 各个服务的可选参数
+	Options interface {
+		// option 的使用者可以通过注册 listener 来响应 config 的运行时变更
+		OnChange(listener OnChangeFunc)
 	}
 )
 
+// 将 config path 对应的数据绑定给 options 然后注入 IoC 容器
 func DI_ConfigOptions[insType any](
 	services di.ServiceCollector,
 	path string,
