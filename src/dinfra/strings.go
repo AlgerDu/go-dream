aa3d5c5ab47d04de06c678f3f6aa57d982e41329
AlgerDu <AlgerDu@outlook.com>
2023-12-04 16:13:55 +0800

    添加两个 string 的扩展方法：Strings_FirstToUpper 和 DerefString

--- src/dinfra/strings.go
+++ src/dinfra/strings.go
@@ -13,3 +13,25 @@ func Strings_FirstToLower(str string) string {
 	}
 	return string(s)
 }
+
+func Strings_FirstToUpper(str string) string {
+	s := []rune(str)
+	if len(s) == 0 {
+		return str
+	}
+	if s[0] >= 65 && s[0] <= 90 {
+		return str
+	}
+	if s[0] >= 97 && s[0] <= 122 {
+		s[0] = s[0] - 32
+	}
+	return string(s)
+}
+
+func DerefString(s *string) string {
+	if s != nil {
+		return *s
+	}
+
+	return ""
+}
