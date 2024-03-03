229ca518ef1df8b34612ba2c084c25ca157a05d4
AlgerDu <AlgerDu@outlook.com>
2024-03-03 11:19:32 +0800

    新定义 cache 扩展方法，尝试下好不好用

--- src/dinfra/cache.go
+++ src/dinfra/cache.go
@@ -19,7 +19,7 @@ type (
 		Set(key string, value any, expire int64) error
 		Get(key string) (CacheValue, error)
 
-		SetH(key string, values map[string]any, expire int64) error
+		SetH(key string, values map[string]any, expire int64) error // TODO 这里不应该直接定义为 map，可能是 struct 使用 hash 存储；其实需要处理
 		GetH(key string, index string) (CacheValue, error)
 
 		Lock(key string)
@@ -38,6 +38,8 @@ type (
 
 	CacheHValueUpdateHandler[ValueType any] func(value ValueType) (map[string]any, *CacheUpdateOptions, error)
 	CacheHValueUpdater[ValueType any]       func(handler CacheHValueUpdateHandler[ValueType]) error
+
+	CacheValueUpdater2[ValueType any] func(value *ValueType, expire int64, options ...func(value ValueType) ValueType) error
 )
 
 func valueToType[ValueType any](value CacheValue) (ValueType, error) {
@@ -99,6 +101,54 @@ func UseCache[ValueType any](cacher Cacher, key string) (CacheValueGetter[ValueT
 	return getter, updater
 }
 
+func UseCache2[ValueType any](cacher Cacher, key string) (CacheValueGetter[ValueType], CacheValueUpdater2[ValueType]) {
+
+	getter := func(nilHandler CacheNilHandler[ValueType]) (ValueType, error) {
+		cacheValue, err := cacher.Get(key)
+		if err == nil {
+			return valueToType[ValueType](cacheValue)
+		}
+
+		if err == ErrNilCache && nilHandler != nil {
+			value, err := nilHandler()
+			if err == nil {
+				cacher.Set(key, value, 0)
+			}
+			return value, err
+		}
+
+		var zero ValueType
+		return zero, err
+	}
+
+	updater := func(value *ValueType, expire int64, options ...func(value ValueType) ValueType) error {
+		cacher.Lock(key)
+		defer cacher.Unlock(key)
+
+		var toUpdate ValueType
+
+		if value == nil {
+			cacheValue, err := getter(nil)
+			if err != nil {
+				return err
+			}
+			toUpdate = cacheValue
+		} else {
+			toUpdate = *value
+		}
+
+		if len(options) > 1 {
+			for _, handler := range options {
+				toUpdate = handler(toUpdate)
+			}
+		}
+
+		return cacher.Set(key, value, expire)
+	}
+
+	return getter, updater
+}
+
 func UseHCache[ValueType any](cacher Cacher, key string) (CacheValueGetter[ValueType], CacheHValueUpdater[ValueType]) {
 	getter := func(nilHandler CacheNilHandler[ValueType]) (ValueType, error) {
 		cacheValue, err := cacher.Get(key)
@@ -138,6 +188,59 @@ func UseHCache[ValueType any](cacher Cacher, key string) (CacheValueGetter[Value
 		}
 
 		return cacher.SetH(key, indexValues, expire)
+	}
+
+	return getter, updater
+}
+
+func UseHCache2[ValueType any](cacher Cacher, key string) (CacheValueGetter[ValueType], CacheValueUpdater2[ValueType]) {
+
+	getter := func(nilHandler CacheNilHandler[ValueType]) (ValueType, error) {
+		cacheValue, err := cacher.Get(key)
+		if err == nil {
+			return valueToType[ValueType](cacheValue)
+		}
+
+		if err == ErrNilCache && nilHandler != nil {
+			value, err := nilHandler()
+			if err == nil {
+				cacher.Set(key, value, 0)
+			}
+			return value, err
+		}
+
+		var zero ValueType
+		return zero, err
+	}
+
+	updater := func(value *ValueType, expire int64, options ...func(value ValueType) ValueType) error {
+		cacher.Lock(key)
+		defer cacher.Unlock(key)
+
+		var toUpdate ValueType
+
+		if value == nil {
+			cacheValue, err := getter(nil)
+			if err != nil {
+				return err
+			}
+			toUpdate = cacheValue
+		} else {
+			toUpdate = *value
+		}
+
+		if len(options) > 1 {
+			for _, handler := range options {
+				toUpdate = handler(toUpdate)
+			}
+		}
+
+		tmp, ok := any(toUpdate).(map[string]any)
+		if !ok {
+			panic("it is not map, HACK 后续修改")
+		}
+
+		return cacher.SetH(key, tmp, expire)
 	}
 
 	return getter, updater
@@ -183,6 +286,54 @@ func UseHCacheIndex[ValueType any](cacher Cacher, key string, index string) (Cac
 		}
 
 		return cacher.SetH(key, map[string]any{index: value}, expire)
+	}
+
+	return getter, updater
+}
+
+func UseHCacheIndex2[ValueType any](cacher Cacher, key string, index string) (CacheValueGetter[ValueType], CacheValueUpdater2[ValueType]) {
+
+	getter := func(nilHandler CacheNilHandler[ValueType]) (ValueType, error) {
+		cacheValue, err := cacher.GetH(key, index)
+		if err == nil {
+			return valueToType[ValueType](cacheValue)
+		}
+
+		if err == ErrNilCache && nilHandler != nil {
+			value, err := nilHandler()
+			if err == nil {
+				cacher.SetH(key, map[string]any{index: value}, 0)
+			}
+			return value, err
+		}
+
+		var zero ValueType
+		return zero, err
+	}
+
+	updater := func(value *ValueType, expire int64, options ...func(value ValueType) ValueType) error {
+		cacher.Lock(key)
+		defer cacher.Unlock(key)
+
+		var toUpdate ValueType
+
+		if value == nil {
+			cacheValue, err := getter(nil)
+			if err != nil {
+				return err
+			}
+			toUpdate = cacheValue
+		} else {
+			toUpdate = *value
+		}
+
+		if len(options) > 1 {
+			for _, handler := range options {
+				toUpdate = handler(toUpdate)
+			}
+		}
+
+		return cacher.SetH(key, map[string]any{index: toUpdate}, expire)
 	}
 
 	return getter, updater
